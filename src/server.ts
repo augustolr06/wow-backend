cf5d6feaec20785f0bc2591489d66823a4cc002e
Augusto Lazaro <augusto.lr06@gmail.com>
2023-06-08 22:06:55 -0300

    feat: create middleware for cors, error and routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,33 @@
-import express from "express";
+import "express-async-errors";
+
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import { routes } from "./routes";
+import { AppError } from "./errors/AppError";
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use(
+  (err: Error, request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
